bca0f55588feca405dc5758e747d7c443d5baff9
Marcus Reinhardt <marcus.reinhardt@deutschebahn.com>
2021-01-05 09:59:20 +0100

    updated dependencies

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,15 +7,6 @@ var flattenColorPalette = require('tailwindcss/lib/util/flattenColorPalette').de
 module.exports = {
   purge: ["./src/**/*.html", "./src/**/*.vue", "./src/**/*.jsx"],
   theme: {
-    borderWidth: {
-      default: '1px',
-      '0': '0',
-      '2': '2px',
-      '3': '3px',
-      '4': '4px',
-      '6': '6px',
-      '8': '8px'
-    },
     flex: {
       '1': '1 1 0%',
       auto: '1 1 auto',
